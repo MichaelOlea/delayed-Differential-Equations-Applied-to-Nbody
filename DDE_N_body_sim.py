6b76a697d5ac28ca78a2028b1c1081c58c3e2267
michael <michaeloleagonzalez@gmail.com>
2025-03-31 20:58:27 -0700

    added yoshida

--- DDE_N_body_sim.py
+++ DDE_N_body_sim.py
@@ -117,17 +117,96 @@ class NBodySimulation:
 
 
     def rung_kutta_4(self):
-        raise NotImplementedError("have not done this bit yet.")
+        """"
+        implmetion of 4th order runge kutta methods
+        """
+
+        # copy current states
+        current_positions = self.positions.copy()
+        current_velocities = self.velocities.copy()
+
+        # calculate k4 term
+        k1_acc = self.calculate_acceleration(current_positions)
+        k1_vel = current_velocities.copy()
+
+        # calculate k2 term
+        k2_pos = current_positions + 0.5 * self.dt * k1_vel
+        k2_acc = self.calculate_acceleration(k2_pos)
+        k2_vel = current_velocities + 0.5 * self.dt *k1_acc
+    
+        # calculate k3 term
+        k3_pos = current_positions + 0.5 * self.dt * k2_vel
+        k3_acc = self.calculate_acceleration(k3_pos)
+        k3_vel = current_velocities + 0.5 * self.dt *k2_acc
+
+        # calculate k4 term
+        k4_pos = current_positions + self.dt * k3_vel
+        k4_acc = self.calculate_acceleration(k4_pos)
+        k4_vel = current_velocities + self.dt *k3_acc
+
+        # update position and velocities
+        self.positions += self.dt / 6.0 * (k1_vel + 2 * k2_vel + 2 * k3_vel + k4_vel)
+        self.velocities += self.dt / 6.0 * (k1_acc + 2 * k2_acc + 2 * k3_acc + k4_acc)
+
+        # update bodies array
+        for i in range(self.num_bodies):
+            self.bodies[i]['position'] = self.positions[i]
+            self.bodies[i]['velocity'] = self.velocities[i]
+
+    def yoshida(self):
+        """
+        Implmenation of yoshida integrator
+        """
+        # yoshida coefficients
+        cr2 = 2 ** (1/3)
+        w0 = -cr2 / (2 - cr2)
+        w1 = 1 / (2 - cr2)
+        c1 = w1 / 2
+        c2 = (w0 + w1) / 2
+
+        # copy current states
+        positions = self.positions.copy()
+        velocities = self.velocities.copy()
+
+        # step 1
+        positions += self.dt * c1 * velocities
+        accelerations = self.calculate_acceleration(positions)
+        velocities += self.dt * w1 * accelerations
+
+        # step 2
+        positions += self.dt * c2 * velocities
+        accelerations = self.calculate_acceleration(positions)
+        velocities += self.dt * w0 * accelerations
+        
+        # step 3
+        positions += self.dt * c2 * velocities
+        accelerations = self.calculate_acceleration(positions)
+        velocities += self.dt * w1 * accelerations
+
+        # step 4
+        positions += self.dt * c1 * velocities
+
+        # update lass variables
+        self.positions = positions
+        self.velocities = velocities
+
+        # update bodies array
+        for i in range(self.num_bodies):
+            self.bodies[i]['position'] = self.positions[i]
+            self.bodies[i]['velocity'] = self.velocities[i]
     
     def choose_integrator(self):
         """
         A general function that let you choose which integrator to apply (will adee more integrators later).
         """
-        self.calculate_acceleration()
         if self.integrator == "euler":
             self.euler()
+        elif self.integrator == "rk4":
+            self.rung_kutta_4()
+        elif self.integrator == "yoshida":
+            self.yoshida()
         else:
-            raise ValueError(f"unknown integrator: {self.integrator}")
+            raise ValueError(f"Unknown integrator: {self.integrator}")
         
     def run(self, frames):
         """
@@ -147,16 +226,21 @@ class NBodySimulation:
         """
 
         history = []
+        history.append(self.positions.copy()) # add initla positions 
         
-        if self.integrator == "euler":
-            for i in range(frames):
+        for i in range(frames):
+            if self.integrator == "euler":
                 self.euler()
-                history.append(self.positions.copy())
-            return np.array(history)
-        elif self.integrator == "rk4":
-            raise NotImplementedError("have not done this bit yet.") 
-        else:
-            raise ValueError(f"Unkiwn integrator: {self.integrator}")
+            elif self.integrator == "rk4":
+                self.rung_kutta_4()
+            elif self.integrator == "yoshida":
+                self.yoshida()
+            else:
+                raise ValueError(f"Unknown integrator: {self.integrator}")
+            
+            history.append(self.positions.copy())
+
+        return np.array(history)
     
     @classmethod
     def load_data(cls, file_name, G = 1, softening = 0, dt = 0.1, integrator = "euler" ):
@@ -196,10 +280,10 @@ class NBodySimulation:
         return sim
 
 # setup simulation
-sim = NBodySimulation.load_data("data1.csv", dt=0.01, integrator='euler')
+sim = NBodySimulation.load_data("data1.csv", dt=0.01, integrator='yoshida')
 
 # Run simulation
-position_history = sim.run(frames=500)
+position_history = sim.run(frames=1000)
 
 # setup plot
 fig = plt.figure()
